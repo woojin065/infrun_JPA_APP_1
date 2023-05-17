45532c6672f3819232065470db66c8a9a4427884
woojin065 <123820750+woojin065@users.noreply.github.com>
2023-05-17 17:24:26 +0900

    프로젝트 생성

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -36,4 +36,11 @@ public class MemberController {
         memberService.join(member);
         return "redirect:/";
     }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
